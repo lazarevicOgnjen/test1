c54f63b7c2124a48c681e2146385d379e0c38374
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-10-25 00:24:57 +0200

    Update main.py

--- main.py
+++ main.py
@@ -97,6 +97,26 @@ try:
     responseAOR1.screenshot('aor1.png')
 
 
+    # lp
+
+    page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=139&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2020&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=")
+    time.sleep(3)
+    
+    responseLP = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    novosti_markdownLP = responseLP.text
+
+    with open("lp.md", "w") as novosti_fileLP:
+        novosti_fileLP.write(novosti_markdownLP)
+
+    heightLP = responseLP.size['height']
+    widthLP = responseLP.size['width']
+    desired_widthLP = max(widthLP, 1200)  
+    desired_heightLP = min(heightLP, 1000)
+    page_to_scrape.set_window_size(desired_widthLP, desired_heightLP)    
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", responseLP)
+    responseLP.screenshot('lp.png')
+
+
 
 
 
