fc7d137068916074025b7c2fd574b434c37e01fc
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-10-24 22:46:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,87 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+import time
 import os
-import sys
-import requests
-from bs4 import BeautifulSoup
 
-PAGE_URL = os.getenv("PAGE_URL")
-if not PAGE_URL:
-    sys.exit("❌ PAGE_URL environment variable missing")
+# Chrome options
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.binary_location = "/usr/bin/google-chrome"
 
-TARGET_SUBJECTS = [
-    "Логичко пројектовање",
-    "Објектно оријентисано пројектовање",
-    "Објектно оријентисано програмирање",
-    "Структуре података",
-    "Архитектура и организација рачунара 1",
-    "Архитектура и организација рачунара 2",
-    "Програмски језици",
-    "Дискретна математика",
-    "Вероватноћа и статистика",
-    "Матрична израчунавања",
-    "Базе података",
-    "Теорија графова",
-    "Геометријски методи и примене ",
-    "Нумерички алгоритми"
-]
+# Chrome driver setup
+browser_driver = Service('/usr/bin/chromedriver')
 
-response = requests.get(PAGE_URL)
-soup = BeautifulSoup(response.text, "html.parser")
+# Start the browser
+page_to_scrape = webdriver.Chrome(service=browser_driver, options=chrome_options)
 
-table = soup.find("table")
-rows = table.find_all("tr")
+try:
+    page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/login/index.php")
+    time.sleep(2)
 
-filtered_rows = []
-for row in rows:
-    cols = row.find_all("td")
-    if len(cols) >= 4 and cols[3].text.strip() in TARGET_SUBJECTS:
-        filtered_rows.append([col.get_text(strip=True) for col in cols])
+    page_to_scrape.find_element(By.XPATH, "/html/body/div[2]/div[2]/div/div/section/div/div[2]/div/div/div/div/div/div[2]/div[3]/div/a").click()
+    time.sleep(2)
 
-with open("README.md", "w", encoding="utf-8") as f:
-    f.write("| Датум | Време | Шифра | Предмет | Просторија |\n")
-    f.write("|-----|-----|-----|-----|-----|\n")
-    for row in filtered_rows:
-        f.write(" | ".join(row) + "\n")
+    mail = page_to_scrape.find_element(By.XPATH, '//*[@id="i0116"]')
+    mail.send_keys(os.environ['MAIL'])  
+    page_to_scrape.find_element(By.XPATH, '//*[@id="idSIButton9"]').click()
+    time.sleep(2)   
 
-print(f"✅ README.md created with {len(filtered_rows)} matching rows")
+    password = page_to_scrape.find_element(By.XPATH, '//*[@id="i0118"]')
+    password.send_keys(os.environ['PASSWORD'])  
+    page_to_scrape.find_element(By.XPATH, '//*[@id="idSIButton9"]').click()
+    time.sleep(2)
+
+    page_to_scrape.find_element(By.XPATH, '//*[@id="idBtn_Back"]').click()
+    time.sleep(2)
+
+    # bp
+
+    page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=4&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2025&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=")
+    time.sleep(3)
+    
+    responseBP = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    novosti_markdown = responseBP.text
+
+    with open("bp.md", "w") as novosti_file:
+        novosti_file.write(novosti_markdown)
+
+    height = responseBP.size['height']
+    width = responseBP.size['width']
+    desired_width = max(width, 1200)  
+    desired_height = min(height, 1000)
+    page_to_scrape.set_window_size(desired_width, desired_height)    
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", responseBP)
+    responseBP.screenshot('bp.png')
+
+    # oop
+
+    page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=45&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=9&fromyear=2024&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=")
+    time.sleep(3)
+    
+    responseOOP = page_to_scrape.find_element(By.XPATH, '//*[@id="region-main"]')
+    novosti_markdown = responseOOP.text
+
+    with open("oop.md", "w") as novosti_file:
+        novosti_file.write(novosti_markdown)
+
+    height = responseOOP.size['height']
+    width = responseOOP.size['width']
+    desired_width = max(width, 1200)  
+    desired_height = min(height, 1000)
+    page_to_scrape.set_window_size(desired_width, desired_height)    
+    page_to_scrape.execute_script("arguments[0].scrollIntoView(true);", responseOOP)
+    responseOOP.screenshot('oop.png')
+
+
+
+
+
+
+
+finally:
+    # Close the browser
+    page_to_scrape.quit()
