602f7c6b2737b07145670060460f17c8c3e9e533
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-10-25 00:23:08 +0200

    Create lp.py

---
+++ lp.py
@@ -0,0 +1,17 @@
+import os
+from dhooks import Webhook, Embed, File
+
+image2_path = 'lp.png'
+
+WEBHOOK_URL = [os.getenv('WEBHOOK_MAIN')]
+for url in WEBHOOK_URL:
+    hook = Webhook(url)
+
+    embed = Embed(
+        description="**[LP forum je azuriran - click here -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=41&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2025&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)**",
+        color=0x3498DB
+    )
+
+    embed.set_image(url="attachment://lp.png")
+    file = File(image2_path, name="lp.png")
+    hook.send("@everyone 📢 LP", embed=embed, file=file)
