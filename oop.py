944b3be76bdc3fe8bf8123bc64e60352f5037842
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-10-24 22:49:43 +0200

    Create oop.py

---
+++ oop.py
@@ -0,0 +1,17 @@
+import os
+from dhooks import Webhook, Embed, File
+
+image2_path = 'oop.png'
+
+WEBHOOK_URL = [os.getenv('WEBHOOK_MAIN')]
+for url in WEBHOOK_URL:
+    hook = Webhook(url)
+
+    embed = Embed(
+        description="**[OOP forum je azuriran - click here -](https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=45&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=9&fromyear=2024&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=)**",
+        color=0x3498DB
+    )
+
+    embed.set_image(url="attachment://oop.png")
+    file = File(image2_path, name="oop.png")
+    hook.send("@everyone 📢 OOP", embed=embed, file=file)
