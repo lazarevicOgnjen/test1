3fec5ee490538a59591d2b6772eb413ab66b0d31
Lazarević Ognjen <lazarevic.ognjen.elfak@gmail.com>
2025-10-24 23:34:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ try:
 
     page_to_scrape.find_element(By.XPATH, '//*[@id="idBtn_Back"]').click()
     time.sleep(2)
-
+    
     # bp
 
     page_to_scrape.get("https://cs.elfak.ni.ac.rs/nastava/mod/forum/search.php?id=4&words=&phrase=&notwords=&fullwords=&timefromrestrict=1&fromday=1&frommonth=1&fromyear=2025&fromhour=0&fromminute=0&hfromday=0&hfrommonth=0&hfromyear=0&hfromhour=0&hfromminute=0&htoday=1&htomonth=1&htoyear=1&htohour=1&htominute=1&forumid=&subject=&user=")
